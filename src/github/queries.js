7d97d72aeec250b98b2c41163a92860f0a8f180c
Rob Brennan <rob@therobbrennan.com>
2019-02-12 18:02:29 -0800

    Added GET_REPOSITORIES_OF_ORGANIZATION query

--- src/github/queries.js
+++ src/github/queries.js
@@ -8,3 +8,20 @@ export const GET_ORGANIZATION = gql`
     }
   }
 `;
+
+export const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query ($organization: String!) {
+    organization(login: $organization) {
+      name
+      url
+      repositories (first: 5) {
+      edges {
+        node {
+          name
+          url
+        }
+      }
+    }
+    }
+  }
+`
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import 'cross-fetch/polyfill';  // Allows us to use the fetch API in a Node.js e
 
 // GraphQL client and related libraries
 import { client as GitHub } from './github/client'
-import { GET_ORGANIZATION } from './github/queries'
+import { GET_REPOSITORIES_OF_ORGANIZATION } from './github/queries'
 
 // Run the query
 GitHub
   .query({
-    query: GET_ORGANIZATION,
+    query: GET_REPOSITORIES_OF_ORGANIZATION,
     variables: {
       organization: 'the-road-to-learn-react',
     }
   })
-  .then(console.log);
+  .then(result => console.log(JSON.stringify(result, null, 2)));
