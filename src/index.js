487373d0a94f730bd7fb44f42db5a469511102ee
Rob Brennan <rob@therobbrennan.com>
2019-02-12 17:55:21 -0800

    Refactored applicaction to use new libraries

--- src/index.js
+++ src/index.js
@@ -1,49 +1,13 @@
 import 'dotenv/config';
-import 'cross-fetch/polyfill';
-import ApolloClient, { gql } from 'apollo-boost';
+import 'cross-fetch/polyfill';  // Allows us to use the fetch API in a Node.js environment
 
-const client = new ApolloClient({
-  uri: process.env.GITHUB_GRAPHQL_ENDPOINT,
-
-  // Use the request property to define a function which has access to
-  // the context of each request made through the Apollo client.
-  //
-  // This is where we will pass our authorization header and personal access token
-  request: operation => {
-    operation.setContext({
-      headers: {
-        authorization: `Bearer ${
-          process.env.GITHUB_PERSONAL_ACCESS_TOKEN
-        }`,
-      },
-    });
-  },
-});
-
-// GraphQL query
-const GET_ORGANIZATION = gql`
-  {
-    organization(login: "the-road-to-learn-react") {
-      name
-      url
-    }
-  }
-`;
+// GraphQL client and related libraries
+import { client as GitHub } from './github/client'
+import { GET_ORGANIZATION } from './github/queries'
 
 // Run the query
-client
+GitHub
   .query({
     query: GET_ORGANIZATION,
   })
   .then(console.log);
-
-/* Sample output
-{ data:
-   { organization:
-      { name: 'The Road to learn React',
-        url: 'https://github.com/the-road-to-learn-react',
-        __typename: 'Organization' } },
-  loading: false,
-  networkStatus: 7,
-  stale: false }
-*/
